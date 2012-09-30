181eed93951aa60fcbff1bf709c1eb975bb1e6ca
dmitry.sergeev <dmitrysergeev@ubuntu.ubuntu-domain>
2012-09-30 21:25:57 +0300

    add static

--- dimlife/urls.py
+++ dimlife/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -14,3 +17,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
